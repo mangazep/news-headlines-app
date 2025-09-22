0368ec6c3d56b5be4598ae579bc722e241206969
al.asepnug <al.asepnug@gmail.com>
2025-09-22 14:36:47 +0700

    update constants

--- app/src/main/java/com/mangazep/newsheadlinesapp/util/Constants.kt
+++ app/src/main/java/com/mangazep/newsheadlinesapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.mangazep.newsheadlinesapp.util
 
 object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val API_KEY = "166c3872e66347dfa3d9eb6ea47726a1"
+    const val API_KEY = "YOUR_APIKEY"
     const val COUNTRY_ID = "us"
 }
