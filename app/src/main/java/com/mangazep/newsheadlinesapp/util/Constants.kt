af344756db10ce4b57193fee7eec61a43be0b216
al.asepnug <al.asepnug@gmail.com>
2025-09-22 22:17:27 +0700

    remove apikey

--- app/src/main/java/com/mangazep/newsheadlinesapp/util/Constants.kt
+++ app/src/main/java/com/mangazep/newsheadlinesapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.mangazep.newsheadlinesapp.util
 
 object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val API_KEY = "YOUR_APIKEY"
+    const val API_KEY = "YOU_APIKEY"
     const val COUNTRY_ID = "us"
 }
