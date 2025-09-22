c51d8b4933a9da8d12cb5cf28f7c0aec758d108f
al.asepnug <al.asepnug@gmail.com>
2025-09-22 14:35:51 +0700

    Reapply "add constants"
    This reverts commit fe6e0650926f3f82089625803631a73bad77230a.

--- app/src/main/java/com/mangazep/newsheadlinesapp/data/api/NewsApiService.kt
+++ app/src/main/java/com/mangazep/newsheadlinesapp/data/api/NewsApiService.kt
@@ -1,13 +1,14 @@
 package com.mangazep.newsheadlinesapp.data.api
 
 import com.mangazep.newsheadlinesapp.data.model.NewsResponse
+import com.mangazep.newsheadlinesapp.util.Constants
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiService {
-    @GET("v2/top-headlines")
+    @GET("top-headlines")
     suspend fun getTopHeadlines(
-        @Query("country") country: String = "us",
+        @Query("country") country: String = Constants.COUNTRY_ID,
         @Query("apiKey") apiKey: String,
         @Query("page") page: Int = 1,
         @Query("pageSize") pageSize: Int = 20
